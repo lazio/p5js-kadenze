09aafd7ef6271445530a608e71fb495265138d5a
lazio <diniovskiy@gmail.com>
2020-02-29 23:49:49 +0200

    add song sketch

--- s2.song/sketch.js
+++ s2.song/sketch.js
@@ -1,7 +1,28 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+
+  var circleSize = 5;
+  var dropShift = 2;
+
+  fill("black");
+  for (var x = 50; x <= width; x += 50) {
+    for (var y = 50; y <= height; y += 50) {
+      for (var z = 0; z <= 10; z += 1) {
+        var dotX = (x * z) / 1.2;
+        var dotY = (y * z) / 3;
+
+        ellipse(dotX, dotY, circleSize, circleSize);
+        triangle(
+          dotX - dropShift,
+          dotY,
+          dotX + dropShift,
+          dotY,
+          dotX,
+          dotY + dropShift + 4
+        );
+      }
+    }
+  }
 }
 
-function draw() {
-
-}
+function draw() {}
